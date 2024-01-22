2bc523b4a9e2d6ddd2cdcd5a89acfb681673fc43
toannt23 <149867771+toannt23@users.noreply.github.com>
2024-01-23 01:01:06 +0700

    PC06149_Buoi8

---
+++ App/Controllers/BaseController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\Render;
+
+
+class BaseController
+{
+    function __construct()
+    {
+        $this->load = new Render();
+    }
+
+    public function redirect($url, $refresh = null): void
+    {
+        header('location:' . $url);
+    }
+}
---
+++ App/Views/layouts/client/home_product.php
@@ -0,0 +1,3 @@
+<?php
+var_dump($products);
+echo "<br>";
